9b067d405152877e51d8689acd193e7de80e4f94
Yujing <vit7david@outlook.com>
2020-05-29 11:01:14 +0300

    fix icon not found error

--- unicafe/src/Statistics.js
+++ unicafe/src/Statistics.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import Statistic from "./Statistic"
 const Statistics = ({ statistics }) => {
+    const { good, neutral, bad, all, average, positive } = statistics
     return (
         <div>
             <h1>statistics</h1>
             <table>
                 <tbody>
-                    <Statistic text="good" value={statistics.good}></Statistic>
-                    <Statistic text="neutral" value={statistics.neutral}></Statistic>
-                    <Statistic text="bad" value={statistics.bad}></Statistic>
-                    <Statistic text="all" value={statistics.all}></Statistic>
-                    <Statistic text="average" value={statistics.average}></Statistic>
-                    <Statistic text="positive" value={statistics.positive}></Statistic>
+                    <Statistic text="good" value={good}></Statistic>
+                    <Statistic text="neutral" value={neutral}></Statistic>
+                    <Statistic text="bad" value={bad}></Statistic>
+                    <Statistic text="all" value={all}></Statistic>
+                    <Statistic text="average" value={average}></Statistic>
+                    <Statistic text="positive" value={positive}></Statistic>
                 </tbody>
             </table>
         </div>
